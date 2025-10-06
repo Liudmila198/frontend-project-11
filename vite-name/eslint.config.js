e939a9b2ec4b75017023c77a27f2ffde698e8164
liudmila198 <belokopytova.ln@gmail.com>
2025-10-06 14:05:35 +0300

    4

---
+++ vite-name/eslint.config.js
@@ -0,0 +1,15 @@
+import js from 'eslint-plugin-js';
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'no-unused-vars': 'error',
+      'no-console': 'warn',
+    },
+  },
+];
